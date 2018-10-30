38cca382b7f0dd647fedd6293e26814737df4793
bvsg363 <saiganesh@dawaibox.com>
2018-10-30 17:39:38 +0530

    login and signup basic UI2

--- app/src/main/java/com/example/gani/trekapp/MainActivity.kt
+++ app/src/main/java/com/example/gani/trekapp/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.gani.trekapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,5 +11,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        login_button.setOnClickListener {
+            val loginIntent = Intent(this, SignUpActivity::class.java)
+            startActivity(loginIntent)
+        }
     }
 }
