7542e5f08cae0c0da622570191414b982b857285
bvsg363 <saiganesh@dawaibox.com>
2018-10-31 20:22:25 +0530

    test commit2

--- app/src/main/java/com/example/gani/trekapp/SignUpActivity.kt
+++ app/src/main/java/com/example/gani/trekapp/SignUpActivity.kt
@@ -1,12 +1,20 @@
 package com.example.gani.trekapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        val signupIntent = Intent(this, MainActivity::class.java)
+
+        button2.setOnClickListener {
+            startActivity(signupIntent)
+        }
     }
 }
